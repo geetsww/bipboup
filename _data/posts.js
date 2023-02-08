e8e416da02c962e7267662e792a85486c58cb9b4
Benjamin Geets <bge@tuta.io>
2023-02-08 13:23:53 +0100

    feat(index): all articles

--- _data/posts.js
+++ _data/posts.js
@@ -3,7 +3,7 @@ const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch
 require('dotenv').config()
 
 module.exports = async () =>{
-    const req = await fetch(process.env.API + '/items/posts/?fields=category.name,title,slug,thumbnail,content,date_created', {
+    const req = await fetch(process.env.API + '/items/posts/?fields=category.name,category.id,title,slug,thumbnail,content,date_created,description', {
         method:'GET',
         headers:{
             Authorization: `Bearer ${process.env.TOKEN}`
