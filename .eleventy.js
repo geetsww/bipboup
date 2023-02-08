e074de08466c93ee4bf71fc77a8c67240a151873
Benjamin Geets <bge@tuta.io>
2023-02-08 11:40:17 +0100

    feat(index): hero

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,26 @@
 const EleventyVitePlugin = require("@11ty/eleventy-plugin-vite");
+require('dotenv').config()
+const MarkdownIt = require("markdown-it");
+
 
 module.exports = function(eleventyConfig) {
+
   eleventyConfig.addPlugin(EleventyVitePlugin);
-  eleventyConfig.addFilter("log", function(value) { console.log(value) });
+
+  eleventyConfig.addFilter("log", function(value) { console.log(" --- LOG:",value) });
+  eleventyConfig.addFilter("getImageUrl", function(value){
+    return process.env.API + "/assets/" + value
+  })
+  eleventyConfig.addFilter('parseDateToFrench', function(value){
+      const date = new Date(value)
+      return date.toLocaleDateString('fr', {day:"2-digit",month:'long', year:'numeric'})
+  })
+
   eleventyConfig.addWatchTarget("public/**/*");
-  const MarkdownIt = require("markdown-it");
+  
   const mdRender = new MarkdownIt();
   eleventyConfig.addFilter("markdown", function(rawString) {
     return mdRender.render(rawString);
   });
+
 };
--- _data/posts.js
+++ _data/posts.js
@@ -3,7 +3,7 @@ const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch
 require('dotenv').config()
 
 module.exports = async () =>{
-    const req = await fetch(process.env.API + '/items/posts/', {
+    const req = await fetch(process.env.API + '/items/posts/?fields=category.name,title,slug,thumbnail,content,date_created', {
         method:'GET',
         headers:{
             Authorization: `Bearer ${process.env.TOKEN}`
