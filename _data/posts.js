94018d45752b53786f886156d508e67f4cf1c51f
Benjamin Geets <bge@tuta.io>
2023-02-09 10:33:54 +0100

    feat: sort posts by date

--- _data/posts.js
+++ _data/posts.js
@@ -3,7 +3,7 @@ const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch
 require('dotenv').config()
 
 module.exports = async () =>{
-    const req = await fetch(process.env.API + '/items/posts/?fields=category.name,category.slug,category.id,title,slug,thumbnail,content,date_created,description,tags.*.*', {
+    const req = await fetch(process.env.API + '/items/posts/?fields=category.name,category.slug,category.id,title,slug,thumbnail,content,date_created,description,tags.*.*&sort=-date_created', {
         method:'GET',
         headers:{
             Authorization: `Bearer ${process.env.TOKEN}`
